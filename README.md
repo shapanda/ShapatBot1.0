cc7d679270b8bc381942360cbf4b1b18ec271f3b
Sardar R <70496290+shapanda@users.noreply.github.com>
2021-08-07 17:06:50 +0000

    added tictactoe, updated kill, and snipe

---
+++ README.md
@@ -0,0 +1,5 @@
+This is the code for ShapatBot by Sardar Rahman.
+My socials are @sardarah on TikTok, and @Sha Panda#7557 on Discord. 
+To use ShapatBot, open this link 
+https://discord.com/api/oauth2/authorize?client_id=872345572038832138&permissions=8&scope=bot
+Commands start with shapat and include: kill @person, dicksmall, sami, rafan.
--- main.py
+++ main.py
@@ -1,19 +1,22 @@
+#Imports everything needed
 import discord
 import os
-import time
+import random
+
+
 import discord.ext
 from discord.utils import get
 from discord.ext import commands, tasks
 from discord.ext.commands import has_permissions,  CheckFailure, check
-#^ basic imports for other features of discord.py and python ^
+
 from keep_alive import keep_alive
-
+#====================================Client Setup============================================
 client = discord.Client()
-
-client = commands.Bot(command_prefix = '!') #put your own prefix here
-
-#Make events
-
+#Prefix before every command, specifically "shapat"
+#also names discord.client as just client so we dont have to type "discord.Client" when we want to reference it in our code.
+client = commands.Bot(command_prefix = 'shapat ') 
+client.sniped_messages = {}
+#=====================================Manage Events==========================================
 @client.event
 async def on_ready():
     print("bot online") #will print "bot online" in the console when the bot is online
@@ -25,32 +28,198 @@ async def on_member_join(member):
         f'Hi {member.name}, welcome to my Discord server!'
     )
 
-    
-
-#make the commands
+def __init__(self, bot: commands.Bot):
+        self.bot = bot
+        self.last_msg = None
+#=====================================Fun Commands===========================================
     
 @client.command()
 async def ping(ctx):
-    await ctx.send("pong!") #simple command so that when you type "!ping" the bot will respond with "pong!"
+    await ctx.send("pong!") 
 
 @client.command()
-async def shaptodicksmall(ctx):
+async def dicksmall(ctx):
     await ctx.send("no ur dick is small")
 
 @client.command()
 async def socials(ctx):
     await ctx.send("@sardarrah = tiktok")
 
-async def kick(ctx, member : discord.Member):
-    try:
-        await member.kick(reason=None)
-        await ctx.send("kicked "+member.mention) #simple kick command to demonstrate how to get and use member mentions
-    except:
-        await ctx.send("bot does not have the kick members permission!")
+@client.command()
+async def rafan(ctx):
+    await ctx.send("rafan is a poop")
 
+@client.command()
+async def sami(ctx):
+    await ctx.send("@xxphoenixhunter#0393 WANNA PLAY BRAWL?")
+
+@client.command()
+async def kill(ctx, member : discord.Member):
+    #What this will do is that it will "kill" a said person.
+    await ctx.send("killed "+member.mention)
+
+@kill.error
+async def kill_error(ctx, error):
+    print(error)
+    if isinstance(error, commands.MissingRequiredArgument):
+        await ctx.send("i cant kill air, im sorry")
+    elif isinstance(error, commands.BadArgument):
+        await ctx.send("at least ping someone for me to kill lol")
+#=======================================Snipe================================================
+@client.event
+async def on_message_delete(message):
+    client.sniped_messages[message.guild.id] = (message.content, message.author, message.channel.name, message.created_at)
+
+@client.command()
+async def snipe(ctx):
+    try:
+        contents, author, channel_name, time = client.sniped_messages[ctx.guild.id]
+        
+    except:
+        await ctx.channel.send("Couldn't find a message to snipe!")
+        return
+
+    embed = discord.Embed(description=contents, color=discord.Color.purple(), timestamp=time)
+    embed.set_author(name=f"{author.name}#{author.discriminator}", icon_url=author.avatar_url)
+    embed.set_footer(text=f"Deleted in : #{channel_name}")
+
+    await ctx.channel.send(embed=embed)
+#======================================TicTacToe=============================================
+player1 = ""
+player2 = ""
+turn = ""
+gameOver = True
+
+board = []
+
+winningConditions = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+]
+
+@client.command()
+async def tictactoe(ctx, p1: discord.Member, p2: discord.Member):
+    global count
+    global player1
+    global player2
+    global turn
+    global gameOver
+
+    if gameOver:
+        global board
+        board = [":white_large_square:", ":white_large_square:", ":white_large_square:",
+                 ":white_large_square:", ":white_large_square:", ":white_large_square:",
+                 ":white_large_square:", ":white_large_square:", ":white_large_square:"]
+        turn = ""
+        gameOver = False
+        count = 0
+
+        player1 = p1
+        player2 = p2
+
+        # print the board
+        line = ""
+        for x in range(len(board)):
+            if x == 2 or x == 5 or x == 8:
+                line += " " + board[x]
+                await ctx.send(line)
+                line = ""
+            else:
+                line += " " + board[x]
+
+        # determine who goes first
+        num = random.randint(1, 2)
+        if num == 1:
+            turn = player1
+            await ctx.send("It is <@" + str(player1.id) + ">'s turn.")
+        elif num == 2:
+            turn = player2
+            await ctx.send("It is <@" + str(player2.id) + ">'s turn.")
+    else:
+        await ctx.send("A game is already in progress! Finish it before starting a new one.")
+
+@client.command()
+async def place(ctx, pos: int):
+    global turn
+    global player1
+    global player2
+    global board
+    global count
+    global gameOver
+
+    if not gameOver:
+        mark = ""
+        if turn == ctx.author:
+            if turn == player1:
+                mark = ":regional_indicator_x:"
+            elif turn == player2:
+                mark = ":o2:"
+            if 0 < pos < 10 and board[pos - 1] == ":white_large_square:" :
+                board[pos - 1] = mark
+                count += 1
+
+                # print the board
+                line = ""
+                for x in range(len(board)):
+                    if x == 2 or x == 5 or x == 8:
+                        line += " " + board[x]
+                        await ctx.send(line)
+                        line = ""
+                    else:
+                        line += " " + board[x]
+
+                checkWinner(winningConditions, mark)
+                print(count)
+                if gameOver == True:
+                    await ctx.send(mark + " wins!")
+                elif count >= 9:
+                    gameOver = True
+                    await ctx.send("It's a tie!")
+
+                # switch turns
+                if turn == player1:
+                    turn = player2
+                elif turn == player2:
+                    turn = player1
+            else:
+                await ctx.send("Be sure to choose an integer between 1 and 9 (inclusive) and an unmarked tile.")
+        else:
+            await ctx.send("It is not your turn.")
+    else:
+        await ctx.send("Please start a new game using the !tictactoe command.")
+
+
+def checkWinner(winningConditions, mark):
+    global gameOver
+    for condition in winningConditions:
+        if board[condition[0]] == mark and board[condition[1]] == mark and board[condition[2]] == mark:
+            gameOver = True
+
+@tictactoe.error
+async def tictactoe_error(ctx, error):
+    print(error)
+    if isinstance(error, commands.MissingRequiredArgument):
+        await ctx.send("Please mention 2 players for this command.")
+    elif isinstance(error, commands.BadArgument):
+        await ctx.send("Please make sure to mention/ping players (ie. <@688534433879556134>).")
+
+@place.error
+async def place_error(ctx, error):
+    if isinstance(error, commands.MissingRequiredArgument):
+        await ctx.send("Please enter a position you would like to mark.")
+    elif isinstance(error, commands.BadArgument):
+        await ctx.send("Please make sure to enter an integer.")
+#============================================================================================
+
+#============================================================================================
+#============================================================================================
 keep_alive()
-token = os.environ.get("TOKEN")
+token = os.environ.get("TOKEN") 
 client.run(token)
-client.run(os.getenv("TOKEN")) #get your bot token and create a key named `TOKEN` to the secrets panel then paste your bot token as the value. 
-#to keep your bot from shutting down use https://uptimerobot.com then create a https:// monitor and put the link to the website that appewars when you run this repl in the monitor and it will keep your bot alive by pinging the flask server
-#enjoy!
+client.run(os.getenv("TOKEN")) 
